5b17f3332ae949588e09c3ceb446a029e9a1509e
r0astPiGGy <1forzyg1@gmail.com>
2024-03-06 17:55:31 +0300

    Add key movements, selection/deselection

--- src/js/View.js
+++ src/js/View.js
@@ -1,10 +1,11 @@
 const grid = document.querySelector("#grid")
-
 const viewModel = ViewModel()
 
 function start() {
     viewModel.setStateUpdateListener(onViewStateUpdated)
     viewModel.setGridSize(4)
+
+    document.addEventListener("keydown", handleKeyPress)
 }
 
 function onViewStateUpdated(viewState) {
@@ -22,15 +23,43 @@ function createRow(cells) {
 
 function createCell(state, x, y) {
     const cell = document.createElement("div")
+
     cell.classList.add("cell")
     cell.dataset.x = x
     cell.dataset.y = y
+
+    if (state.selected) {
+        cell.classList.add("selected")
+    }
 
     cell.textContent = state.content
 
     cell.addEventListener("click", handleCellClick)
 
     return cell
+}
+
+function handleKeyPress(event) {
+    let shouldCancelEvent = true
+
+    switch (event.key) {
+        case "ArrowUp": viewModel.onMove(UP); break;
+        case "ArrowDown": viewModel.onMove(DOWN); break;
+        case "ArrowRight": viewModel.onMove(RIGHT); break;
+        case "ArrowLeft": viewModel.onMove(LEFT); break;
+        case "Escape": viewModel.onDeselect(); break;
+        case "Enter":
+        case " ":
+            viewModel.onSelectedInteract()
+            break;
+        default:
+            shouldCancelEvent = false;
+            break;
+    }
+
+    if (shouldCancelEvent) {
+        event.preventDefault()
+    }
 }
 
 function handleCellClick(event) {
--- src/js/ViewModel.js
+++ src/js/ViewModel.js
@@ -7,6 +7,14 @@ const ViewModel = function() {
         return viewState.grid[y][x]
     }
 
+    function getWidth() {
+        return viewState.grid[0].length
+    }
+
+    function getHeight() {
+        return viewState.grid.length
+    }
+
     function setStateUpdateListener(listener) {
         stateUpdateListener = listener
     }
@@ -42,14 +50,60 @@ const ViewModel = function() {
     function onMove(direction) {
         if (direction.length === undefined || direction.length !== 2) return
 
-        const xOffset = direction[0]
-        const yOffset = direction[1]
+        const xOffset = +direction[0]
+        const yOffset = +direction[1]
+
+        let point = viewState.selectedPoint
+
+        if (point === null) {
+            point = pointOf(0, 0)
+        } else {
+            point = pointOf(point.x, point.y)
+        }
+
+        point.x += xOffset
+        point.y += yOffset
+
+        if (point.x < 0 || point.x >= getWidth() || point.y < 0 || point.y >= getHeight()) {
+            return
+        }
+
+        const prevPoint = viewState.selectedPoint
+
+        if (prevPoint !== null) {
+            getCellStateAt(prevPoint.x, prevPoint.y).selected = false
+        }
+
+        viewState.selectedPoint = point
+        getCellStateAt(point.x, point.y).selected = true
+
+        fireStateUpdate()
+    }
+
+    function onSelectedInteract() {
+        const point = viewState.selectedPoint
+        if (point === null) return
+
+        onCellClick(point.x, point.y)
+    }
+
+    function onDeselect() {
+        const point = viewState.selectedPoint
+
+        if (point === null) return
+
+        viewState.selectedPoint = null
+
+        getCellStateAt(point.x, point.y).selected = false
+        fireStateUpdate()
     }
 
     return {
         setGridSize: setGridSize,
         setStateUpdateListener: setStateUpdateListener,
         onMove: onMove,
+        onSelectedInteract: onSelectedInteract,
+        onDeselect: onDeselect,
         onCellClick: onCellClick
     }
 }
